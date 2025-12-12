0a03c653b6e9050d436879780260c9230f24f3dd
Matthias Personnaz <matthias.personnaz@hotmail.fr>
2025-12-13 00:42:20 +0100

    Yet some otherreading optimizations with polars and clever conversions

--- utils.py
+++ utils.py
@@ -9,26 +9,21 @@ class SocialGraph:
         self.source_file = source_file
         self.graph: nx.Graph = nx.Graph()
         self.individuals: set[str] = set()
-    
+
     def build(self, limit: int = 0):
-        # Read CSV with pandas for use with networkx.from_pandas_edgelist
-        df = pd.read_csv(self.source_file)
-        
-        if limit > 0:
-            df = df.head(limit)
-        
+        # Read CSV efficiently with polars (multi-threaded)
+        df_polars = pl.read_csv(self.source_file, n_rows=limit, n_threads=4)
+
+        # Convert to pandas DataFrame for use with networkx.from_pandas_edgelist
+        df_pandas = df_polars.to_pandas(use_pyarrow_extension_array=True)
+
         # Use networkx.from_pandas_edgelist to create graph directly
         self.graph = nx.from_pandas_edgelist(
-            df, 
-            source='user_a', 
-            target='user_b', 
-            create_using=nx.Graph()
+            df_pandas, source="user_a", target="user_b", create_using=nx.Graph()
         )
-        
+
         # Update individuals set with all nodes from the graph
         self.individuals.update(self.graph.nodes())
-
-
 
 
 class CandidateDataset:
@@ -40,7 +35,6 @@ class CandidateDataset:
         self.df_train: pl.DataFrame = pl.DataFrame()
         self.df_test: pl.DataFrame = pl.DataFrame()
 
-    
     def build(self, limit: int = 0):
-        self.df_train = pl.read_csv(self.source_train)
-        self.df_test = pl.read_csv(self.source_test)
+        self.df_train = pl.read_csv(self.source_train, n_rows=limit, n_threads=4)
+        self.df_test = pl.read_csv(self.source_test, n_rows=limit, n_threads=4)
